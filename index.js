62bf83c742b056f502b364d09c4ed27c42292895
JetsadaWijit <jetsadawijit@outlook.com>
2026-09-14 10:43:33 +0000

    chore: add simple value and function

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('Hello, TypeScript!');
+console.log("Hello, TypeScript!");
+let num1 = 20;
+/* num1 = "Arm"; */
+/* num1 = 5.7 */
+let num2 = 47;
+let myName = "Owen";
+function add(x, y) {
+    let answer = x + y;
+    return answer;
+}
+console.log("Hello " + myName + " number: " + add(num1, num2));
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, TypeScript!');
+console.log("Hello, TypeScript!");
+
+let num1: number = 20;
+/* num1 = "Arm"; */
+/* num1 = 5.7 */
+let num2: number = 47;
+
+let myName: string = "Owen";
+
+function add(x: number, y: number): number {
+  let answer = x + y;
+  return answer;
+}
+
+console.log("Hello " + myName + " number: " + add(num1, num2));
